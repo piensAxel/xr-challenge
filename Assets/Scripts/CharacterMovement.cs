7d78d9954f901f08c0485bfe77a21aa1d8f4dac2
AxelPiens <axel.piens@gmail.com>
2022-01-12 18:06:28 +0100

    Add character animations

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,10 +9,14 @@ public class CharacterMovement : MonoBehaviour
     private float _moveSpeed = 10.0f;
     private Rigidbody _rb;
     private Vector3 _moveDir;
+
+    private Animator _anim;
+
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,10 +24,14 @@ public class CharacterMovement : MonoBehaviour
     {
         _moveDir.x = Input.GetAxisRaw("Horizontal");
         _moveDir.z = Input.GetAxisRaw("Vertical");
+        _anim.SetFloat("Horizontal", _moveDir.x);
+        _anim.SetFloat("Vertical", _moveDir.z);
+        _anim.SetFloat("Speed", _moveDir.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {   
         _rb.velocity = _moveDir.normalized * _moveSpeed;
+        
     }
 }
